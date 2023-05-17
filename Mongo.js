b3f0e4979e8785503b40bd55219902a96fd83936
Epiczone89 <71631090+Epiczone89@users.noreply.github.com>
2023-05-17 11:10:02 +0300

    update

--- Mongo.js
+++ Mongo.js
@@ -8,22 +8,35 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://Admin:Qwerty@training.wpdz4xf.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://Admin:${password}@training.wpdz4xf.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: 
+  {
+    type: String,
+    minlength: 5,
+    required: true
+  },
+  date: Date,
   important: Boolean,
 })
 
- const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
   content: 'HTML is Easy',
+  date: new Date(),
   important: true,
 })
+
+// Tallentaa uuden noten
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
 
 Note.find({}).then(result => 
     {
@@ -32,9 +45,3 @@ Note.find({}).then(result =>
         })
         mongoose.connection.close()
     })
-
-    // Tallentaa uuden noten
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
